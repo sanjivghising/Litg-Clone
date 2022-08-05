aa4a47a273d964b9166f12117411fd173c43d867
MakeITLazy <107746675+sanjivghising@users.noreply.github.com>
2022-08-05 09:46:01 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,57 @@
+document.getElementById("myIA").addEventListener("mouseover", function() {
+    document.getElementById("myIA").style.backgroundColor = "#0C1740";
+});
+
+document.getElementById("myIA").addEventListener("mouseout", function() {
+    document.getElementById("myIA").style.backgroundColor = "#46517a";
+});
+
+
+
+document.getElementById("myID").addEventListener("mouseover", function() {
+  document.getElementById("myID").style.backgroundColor = "#0C1740";
+});
+
+document.getElementById("myID").addEventListener("mouseout", function() {
+  document.getElementById("myID").style.backgroundColor = "#46517a";
+});
+function color() {
+document.getElementById("myID").style.backgroundColor = "#0C1740";
+
+}
+
+document.getElementById("myIM").addEventListener("mouseover", function() {
+  document.getElementById("myIM").style.backgroundColor = "#0C1740";
+});
+
+document.getElementById("myIM").addEventListener("mouseout", function() {
+  document.getElementById("myIM").style.backgroundColor = "#46517a";
+});
+function color() {
+document.getElementById("myIM").style.backgroundColor = "#0C1740";
+
+}
+
+  document.getElementById("myIC").addEventListener("mouseover", function() {
+    document.getElementById("myIC").style.backgroundColor = "#0C1740";
+});
+
+document.getElementById("myIC").addEventListener("mouseout", function() {
+    document.getElementById("myIC").style.backgroundColor = "#46517a";
+});
+function color() {
+  document.getElementById("myIC").style.backgroundColor = "#0C1740";
+
+}
+
+  document.getElementById("myIK").addEventListener("mouseover", function() {
+    document.getElementById("myIK").style.backgroundColor = "#0C1740";
+});
+
+document.getElementById("myIK").addEventListener("mouseout", function() {
+    document.getElementById("myIK").style.backgroundColor = "#46517a";
+});
+function color() {
+  document.getElementById("myIK").style.backgroundColor = "#0C1740";
+
+}
